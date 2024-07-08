e356254db83399afc05d1fbbd836308c323487c0
Guy Liba <guy33liba@gmail.com>
2024-07-08 16:31:52 +0200

    fdas

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,14 +1,14 @@
 import { useState } from "react"
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
-import { useAuthContext } from '../hooks/useAuthContext'
+import { useAuthContext } from "../hooks/useAuthContext"
 
 const WorkoutForm = () => {
   const { dispatch } = useWorkoutsContext()
   const { user } = useAuthContext()
 
-  const [title, setTitle] = useState('')
-  const [load, setLoad] = useState('')
-  const [reps, setReps] = useState('')
+  const [title, setTitle] = useState("")
+  const [load, setLoad] = useState("")
+  const [reps, setReps] = useState("")
   const [error, setError] = useState(null)
   const [emptyFields, setEmptyFields] = useState([])
 
@@ -16,19 +16,19 @@ const WorkoutForm = () => {
     e.preventDefault()
 
     if (!user) {
-      setError('You must be logged in')
+      setError("You must be logged in")
       return
     }
 
-    const workout = {title, load, reps}
+    const workout = { title, load, reps }
 
-    const response = await fetch('/api/workouts', {
-      method: 'POST',
+    const response = await fetch("/api/workouts", {
+      method: "POST",
       body: JSON.stringify(workout),
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${user.token}`
-      }
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
     })
     const json = await response.json()
 
@@ -37,12 +37,12 @@ const WorkoutForm = () => {
       setEmptyFields(json.emptyFields)
     }
     if (response.ok) {
-      setTitle('')
-      setLoad('')
-      setReps('')
+      setTitle("")
+      setLoad("")
+      setReps("")
       setError(null)
       setEmptyFields([])
-      dispatch({type: 'CREATE_WORKOUT', payload: json})
+      dispatch({ type: "CREATE_WORKOUT", payload: json })
     }
   }
 
@@ -50,28 +50,28 @@ const WorkoutForm = () => {
     <form className="create" onSubmit={handleSubmit}>
       <h3>Add a New Workout</h3>
 
-      <label>Excersize Title:</label>
-      <input 
+      <label>Excercise Title:</label>
+      <input
         type="text"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
-        className={emptyFields.includes('title') ? 'error' : ''}
+        className={emptyFields.includes("title") ? "error" : ""}
       />
 
       <label>Load (in kg):</label>
-      <input 
+      <input
         type="number"
         onChange={(e) => setLoad(e.target.value)}
         value={load}
-        className={emptyFields.includes('load') ? 'error' : ''}
+        className={emptyFields.includes("load") ? "error" : ""}
       />
 
       <label>Reps:</label>
-      <input 
+      <input
         type="number"
         onChange={(e) => setReps(e.target.value)}
         value={reps}
-        className={emptyFields.includes('reps') ? 'error' : ''}
+        className={emptyFields.includes("reps") ? "error" : ""}
       />
 
       <button>Add Workout</button>
