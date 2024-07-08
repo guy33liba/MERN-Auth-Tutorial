47b7540e3f505f51b1abcaa4378fcde0627a899b
guy33liba <guy33liba@gmail.com>
2024-07-08 21:00:03 +0300

    sg

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
+require("dotenv").config()
 
-const express = require('express')
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
-const userRoutes = require('./routes/user')
+const express = require("express")
+const mongoose = require("mongoose")
+const workoutRoutes = require("./routes/workouts")
+const userRoutes = require("./routes/user")
 
 // express app
 const app = express()
@@ -17,15 +17,16 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.use('/api/workouts', workoutRoutes)
-app.use('/api/user', userRoutes)
+app.use("/api/workouts", workoutRoutes)
+app.use("/api/user", userRoutes)
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
+      console.log("connected to db & listening on port", process.env.PORT)
     })
   })
   .catch((error) => {
